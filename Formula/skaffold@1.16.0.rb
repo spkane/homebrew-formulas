2e268a53d9a64e56457c86dd21e6f936418b340d
Sean P. Kane <sean@superorbit.al>
2020-12-08 16:54:37 -0800

    initial commit

---
+++ Formula/skaffold@1.16.0.rb
@@ -0,0 +1,26 @@
+class SkaffoldAT1160 < Formula
+  desc "Easy and Repeatable Kubernetes Development"
+  homepage "https://skaffold.dev/"
+  url "https://github.com/GoogleContainerTools/skaffold.git",
+      tag:      "v1.16.0",
+      revision: "dcdf77cd94ec6a69d116c2123dc8727971e8b14c"
+  license "Apache-2.0"
+  head "https://github.com/GoogleContainerTools/skaffold.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "make"
+    bin.install "out/skaffold"
+    output = Utils.safe_popen_read("#{bin}/skaffold", "completion", "bash")
+    (bash_completion/"skaffold").write output
+    output = Utils.safe_popen_read("#{bin}/skaffold", "completion", "zsh")
+    (zsh_completion/"_skaffold").write output
+  end
+
+  test do
+    (testpath/"Dockerfile").write "FROM scratch"
+    output = shell_output("#{bin}/skaffold init --analyze").chomp
+    assert_equal '{"dockerfiles":["Dockerfile"]}', output
+  end
+end
